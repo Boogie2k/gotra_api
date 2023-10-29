0cec3153d0aa623183522895d178630d10d5f8a8
Boogie2k <oghosabenjamin@gmail.com>
2023-10-29 02:10:37 +0100

    'commit'

--- gotra/gotra/settings.py
+++ gotra/gotra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-owr#wdl9^t&*#g!=3zn0=bshipszqjviydv67knmo!t%ugxnj(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["goapi-cqoy.onrender.com"]
 
 
 # Application definition
